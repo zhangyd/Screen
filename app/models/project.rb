4a752f50a73b32e68cc3efc2b8ff8b292b50a613
zhangyd <zhangyd@umich.edu>
2015-09-03 10:28:12 -0400

    fixed minor bugs

--- app/models/project.rb
+++ app/models/project.rb
@@ -10,6 +10,6 @@ class Project < ActiveRecord::Base
 	has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
-  	validates :title, :subtitle, :link, :current, :description, :presence => true
+  	validates :title, :subtitle, :link, :description, :presence => true
 
 end
